f8f2ff38329829d4bfd94085ad8f3f96af7f6381
Jose Antonio Solis <101215363+MrJ8585@users.noreply.github.com>
2024-12-02 18:40:30 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,18 @@ import { env } from "process";
 import react from "@vitejs/plugin-react";
 
 const isDevelopment = env.NODE_ENV === "development";
+const target =  isDevelopment ? "http://localhost:3001" : "https://farmbook85-backend.azurewebsites.net"
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: isDevelopment
-		? {
-				port: 3000,
-				proxy: {
-					"/api": {
-						target: "https://farmbook85-backend.azurewebsites.net", // Backend local
-						changeOrigin: true,
-					},
+	server: {
+			port: 3000,
+			proxy: {
+				"/api": {
+					target: target,
+					changeOrigin: true,
 				},
+			},
 		  }
-		: undefined, // No configura proxy en producción
 });
